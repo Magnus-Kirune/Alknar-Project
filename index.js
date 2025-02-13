ee23cda759f29dd211f1a2e795113636679e0eba
Magnus <magnus.devsystem@gmail.com>
2025-02-13 19:57:37 -0300

    Subindo para o GitHub

---
+++ index.js
@@ -0,0 +1,34 @@
+class Jogador{
+    constructor(name){
+        this.name = name
+        this.id = id
+    }
+
+    function () {
+        //O jogador vai fazer o movimento (vai gastar a ficha).
+    }
+
+}
+
+class Mestre{
+    constructor(name){
+        this.name = name
+    }
+
+    function (){
+        //Adicionar movimento a um jogador.
+    }
+
+    function (){
+        //Retirar um movimento de um jogador.
+    }
+
+    function (){
+        //Resetar turno.
+    }
+
+    function (){
+       //Adicionar ou remover movimento; (no quesito ficha adicional)
+    }
+}
+
